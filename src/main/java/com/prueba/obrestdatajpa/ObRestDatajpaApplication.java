d781884b61fe45ec7ed8108a9818c48ed8d2a6e2
xfernanx <xfernandorguezx@gmail.com>
2024-01-27 16:10:07 +0100

    Se añade autor

--- src/main/java/com/prueba/obrestdatajpa/ObRestDatajpaApplication.java
+++ src/main/java/com/prueba/obrestdatajpa/ObRestDatajpaApplication.java
@@ -2,6 +2,8 @@ package com.prueba.obrestdatajpa;
 
 import java.time.LocalDate;
 
+import com.prueba.obrestdatajpa.Models.Author;
+import com.prueba.obrestdatajpa.Repository.AuthorRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -16,12 +18,17 @@ public class ObRestDatajpaApplication {
 		ApplicationContext context = SpringApplication.run(ObRestDatajpaApplication.class, args);
 
 		BookRepository repository = context.getBean(BookRepository.class);
+		AuthorRepository authorRepository = context.getBean(AuthorRepository.class);
 
 		//CRUD
 		//Crear un libro
 		Book book1 = new Book(null, "Hasta los cojones del pensamiento positivo", "Buenaventura del charco", 450, 19.99, LocalDate.of(2018, 12, 1), true);
 		Book book2 = new Book(null, "La Torre Oscura", "Stephen King", 250, 9.99, LocalDate.of(2015, 06, 12), true);
 
+		//Añadir un autor
+		Author autor1 = new Author(null, "Fernando", "Rodriguez", LocalDate.of(1987,04,24), "España");
+
+		authorRepository.save(autor1);
 		//Almacenar el libro en la BBDD
 		System.out.println("En la base de datos hay: " + repository.count() + " libros");
 		repository.save(book1);
